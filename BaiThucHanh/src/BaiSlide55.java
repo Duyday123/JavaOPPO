a632531a3c9db1b477b366fc6eba8f3caee97cd7
Duyday123 <cen27726@gmail.com>
2024-02-03 22:25:56 +0700

    Slide 55

--- BaiThucHanh/src/BaiSlide55.java
+++ BaiThucHanh/src/BaiSlide55.java
@@ -4,6 +4,7 @@ public class BaiSlide55 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);      
         System.out.println("cac so chia het cho 5 la: ");
+        
   for (int i = 0; i <= 20; i++) {
      if (i % 5 != 0) {
             continue;
