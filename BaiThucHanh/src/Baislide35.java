5dd540298dc674e9a01b51ea8fedebe77d2b5525
Duyday123 <cen27726@gmail.com>
2024-02-03 22:23:38 +0700

    Slide 35

--- BaiThucHanh/src/Baislide35.java
+++ BaiThucHanh/src/Baislide35.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Baislide35 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    
     System.out.print("Nhap so thu nhat : ");
     double num1 = sc.nextDouble();
     System.out.print("Nhap so thu hai : ");
