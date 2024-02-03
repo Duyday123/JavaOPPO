70f3f85ef96d630cd89bc6a251727250b1aa3a79
Duyday123 <cen27726@gmail.com>
2024-02-03 22:25:25 +0700

    Slide 47

--- BaiThucHanh/src/Baislide47.java
+++ BaiThucHanh/src/Baislide47.java
@@ -3,6 +3,7 @@ public class Baislide47 {
   public static void main(String[] args) {
     int number, tong = 0;
     Scanner sc = new Scanner(System.in);      
+    
     do {
      System.out.println("Nhap vao so nguyen bat ky: ");
        number = sc.nextInt();
