167c8c92137af56990c552de06668552d95235fc
Duyday123 <cen27726@gmail.com>
2024-02-03 22:13:47 +0700

    Bai7lab2

--- Lab2/src/Bai7.java
+++ Lab2/src/Bai7.java
@@ -7,7 +7,7 @@ public class Bai7 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
         for (int i =1; i<=20; i++){
-    System.out.println(i);}
+    System.out.println( i );}
     sc.close();
     } 
 }
