c052c55a23e580def21be4d87f41d31aacfcc3d7
Duyday123 <cen27726@gmail.com>
2024-02-03 22:07:46 +0700

    Bai3lab2

--- Lab2/src/Bai3.java
+++ Lab2/src/Bai3.java
@@ -4,9 +4,9 @@ public class Bai3 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(" nhap ten cua ban : ");
+        System.out.print(" moi nhap ten cua ban : ");
         String ten = sc.nextLine();
-        System.out.print(" nhap nam sinh cua ban : ");
+        System.out.print(" moi nhap nam sinh cua ban : ");
         int namsinh = sc.nextInt();
         int tuoi = 2024 - namsinh;
         if(tuoi<16){
