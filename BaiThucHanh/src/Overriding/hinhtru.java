2eaf275fed427db77c92764756c0c7593262e4e5
Duyday123 <cen27726@gmail.com>
2024-04-12 10:03:44 +0700

    Slide 67 lab 3

---
+++ BaiThucHanh/src/Overriding/hinhtru.java
@@ -0,0 +1,7 @@
+package Overriding;
+
+public class hinhtru extends hinhtron {
+    public void xuatthongtin() {
+        System.out.println("Day la Hinh Tru");
+    }
+}
---
+++ BaiThucHanh/src/Overriding/main.java
@@ -0,0 +1,11 @@
+package Overriding;
+
+public class main {
+    public static void main(String[] args) {
+        hinhtron ht = new hinhtron();
+        hinhtru htr = new hinhtru();
+
+        ht.xuatthongtin();
+        htr.xuatthongtin();
+    }
+}
