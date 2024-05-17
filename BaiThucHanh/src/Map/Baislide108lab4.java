0188ce9cce2e7d6e8d0b72b464d4fb20a7e75d65
Duyday123 <cen27726@gmail.com>
2024-05-17 09:54:55 +0700

    Bai slide 108 lab 4

---
+++ BaiThucHanh/src/Map/Baislide108lab4.java
@@ -0,0 +1,24 @@
+package Map;
+
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.Map;
+
+public class Baislide108lab4 {
+    public static void main(String[] args) {
+        TreeMap<Integer, Double> treeMap = new TreeMap<>();
+        treeMap.put(1, 9d);
+        treeMap.put(4, 10.1d);
+        treeMap.put(2, 7.2d);
+        treeMap.put(8, 20.28d);
+        System.out.println("cac phan tu co trong TreeMap : ");
+        Set<Map.Entry<Integer, Double>> setTreeMap = treeMap.entrySet();
+        System.out.println(setTreeMap);
+        treeMap.replace(4, 20.11d);
+        treeMap.replace(2, 7.2d, 7.7d);
+        System.out.println("cac phan tu co trong TreeMap sau khi thay the : ");
+        setTreeMap = treeMap.entrySet();
+        System.out.println(setTreeMap);
+    }
+
+}
