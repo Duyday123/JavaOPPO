03825d1340285399b428084d3a6ba2ce1bc4f6f9
Duyday123 <cen27726@gmail.com>
2024-05-24 07:22:53 +0700

    Bai 3 lab 4

---
+++ Lab4/src/Bai3lab4/Student.java
@@ -0,0 +1,11 @@
+package Bai3lab4;
+
+public class Student {
+        String name;
+        double score;
+    
+        public Student(String name, double score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
